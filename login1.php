bc8cd991527eb4dd00e78f384edd7b30095e9acb
khusheimeda <43701562+khusheimeda@users.noreply.github.com>
2019-11-17 12:01:33 +0530

    backend for login also works

--- login1.php
+++ login1.php
@@ -10,34 +10,36 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 if (isset($_POST['login_btn']))
 {
     $username = mysqli_real_escape_string($db, $_POST['uname']);
-    $psw = mysqli_real_escape_string($db, $_POST['psw']);
-    $enc_password = md5($psw);
+    $pswd = mysqli_real_escape_string($db, $_POST['pswd']);
+    $enc_password = md5($pswd);
+    //$enc_password = password_hash($pswd, PASSWORD_DEFAULT);
     $errors= array();
 }
 
 if (empty($username)) { array_push($errors, "Username is required"); }
-if (empty($psw)) { array_push($errors, "Password is required"); }
+if (empty($pswd)) { array_push($errors, "Password is required"); }
 $user_check_query = "SELECT * FROM lang_first_tb WHERE Username='$username'  LIMIT 1";
 $result = mysqli_query($db, $user_check_query);
 $user = mysqli_fetch_assoc($result);
 
 if ($user && count($errors) == 0) { 
-    echo $enc_password;
-    //if(strcasecmp($user['Username'], $username) === 0 && strcmp($user['Enc_Password'], $enc_password) === 0) {
-    if($user['Username'] == $username && $user['Enc_Password'] == $enc_password) {
-        echo "success";
+    if((strcasecmp($user['Username'], $username) === 0) && strpos($enc_password, $user['Enc_Password']) !== false){
         header('refresh:1; url=main.html');
     }
-   /* else
+    else
     {
-        header('refresh:2; url=login.html');
-    }*/
+        array_push($errors, "Incorrect password.");
+        foreach($errors as $x)
+            echo $x;
+        header('refresh:5; url=login.html');
+    }
 }
 else
 {
-  foreach($errors as $x)
-  echo $x;
-  header('refresh:2; url=login.html');
+    array_push($errors, "User doesn't exist.");
+    foreach($errors as $x)
+        echo $x;
+    header('refresh:5; url=login.html');
 }
 
 ?>
--- register1.php
+++ register1.php
@@ -48,12 +48,12 @@ $result = mysqli_query($db, $user_check_query);
 $user = mysqli_fetch_assoc($result);
 
 if ($user) { // if user exists
-  /*if ($user[$username] === $username) {
+  /*if ($user['Username'] === $username) {
     array_push($errors, "Username already exists");
     echo "User already exists!";
   }
 
-  if ($user[$email] === $email) {
+  if ($user['Email'] === $email) {
     array_push($errors, "email already exists");*/
     array_push($errors, "User already exists");
 }
@@ -61,6 +61,7 @@ if ($user) { // if user exists
 // Finally, register user if there are no errors in the form
 if (count($errors) == 0) {
   $password = md5($psw);//encrypt the password before saving in the database
+  //$password = string md5(string $psw [, bool $raw_output = false ]);
 
   $query = "INSERT INTO lang_first_tb(FullName, Age, Email, Proficient_Language, Current_Language, Username, Enc_Password) VALUES('$name', '$age', '$email', '$pro_language', '$curr_language', '$username', '$password')";
   mysqli_query($db, $query);
